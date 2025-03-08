863a82f99c99c73e5b53b90df7dd9e899fa27c8a
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2025-03-08 12:39:33 +0100

    Update L2.cpp

--- L2/L2.cpp
+++ L2/L2.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
+
+std::ofstream wynik;
 
 void generacjaPodzialow(int n) {
     int* a = new int[n];
@@ -10,7 +14,7 @@ void generacjaPodzialow(int n) {
         b[i] = 1;
     }
 
-    while (a[0] == 1) {
+    while (true) {
         c = n;
 
         while (a[c] == n || a[c] > b[c]) {
@@ -34,18 +38,38 @@ void generacjaPodzialow(int n) {
         }
 
         for (int i = 0; i < n; i++) {
-            std::cout << a[i] << " ";
+            wynik << a[i] << " ";
         }
-        std::cout << std::endl;
+        wynik << std::endl;
     }
 }
 
 int main()
 {
+    wynik.open("wynik.txt");
+    
     int n;
+    auto start = std::chrono::high_resolution_clock::now();
+    auto stop = start;
+
+    std::cout << "Generacja podzialow zbioru - algorytm\n";
     std::cout << "Podaj wielkosc zbioru: \n";
     std::cin >> n;
+
+    if (n <= 1) {
+        std::cout << "Niepoprawne dane wejsciowe\n";
+        return -1;
+    }
+
+    start = std::chrono::high_resolution_clock::now();
     generacjaPodzialow(n);
+    stop = std::chrono::high_resolution_clock::now();
+
+    wynik.close();
+    std::cout << "Wynik zostal zapisany do pliku wynik.txt\n";
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Czas dzialania algorytmu wynosi " << duration.count() << " mikrosekund";
 
     return 0;
 }
