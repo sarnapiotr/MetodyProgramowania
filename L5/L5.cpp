dae6dae8bd047b89c815e32d2cffe4e3df7029d2
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2025-03-28 20:53:23 +0100

    Update L5.cpp

--- L5/L5.cpp
+++ L5/L5.cpp
@@ -7,26 +7,59 @@ int getRand(int min, int max) {
     return num;
 }
 
-int* generateRandomArray(int n) {
-    int* array = new int[n];
-
+void generateRandomArray(int* array, int n) {
     for (int i = 0; i < n; i++) {
         array[i] = getRand(1, n);
     }
-
-    return array;
 }
 
-int* generateHeap(int* array, int n) {
-
+int getParent(int i) {
+    return (i - 1) / 2;
 }
 
-int* sortNonDescending(int* array, int n) {
-
+void shiftUp(int* array, int childIndex) {
+    while (array[childIndex] > array[getParent(childIndex)] && childIndex != 0) {
+        int parentIndex = getParent(childIndex);
+        int temp = array[parentIndex];
+        array[parentIndex] = array[childIndex];
+        array[childIndex] = temp;
+        childIndex = parentIndex;
+    }
 }
 
-int* sortNonAscending() {
+void generateHeap(int* array, int n) {
+    for (int i = 0; i < n; i++) {
+        shiftUp(array, i);
+    }
+}
 
+void sortNonDescending(int* array, int n) {
+    int tempIndex = n;
+
+    for (int i = 0; i < n; i++) {
+        int temp = array[0];
+        array[0] = array[tempIndex - 1];
+        array[tempIndex - 1] = temp;
+        tempIndex--;
+        generateHeap(array, tempIndex);
+    }
+}
+
+void sortNonAscending(int* array, int n) {
+    sortNonDescending(array, n);
+
+    for (int i = 0; i < n / 2; i++) {
+        int temp = array[i];
+        array[i] = array[n - 1 - i];
+        array[n - 1 - i] = temp;
+    }
+}
+
+void printArray(int* array, int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main()
@@ -38,8 +71,12 @@ int main()
     std::cout << "Sortowanie przez kopcowanie\n";
     std::cout << "Podaj rozmiar tablicy do posortowania: \n";
     std::cin >> n;
-    int* array = generateRandomArray(n);
-    array = generateHeap(array, n);
+
+    int* array = new int[n];
+    generateRandomArray(array, n);
+    std::cout << "Tablica przed posortowaniem: \n";
+    printArray(array, n);
+    generateHeap(array, n);
 
     std::cout << "1. Sortowanie niemalejace\n";
     std::cout << "2. Sortowanie nierosnace\n";
@@ -48,17 +85,21 @@ int main()
     int choice;
     std::cin >> choice;
 
+    std::cout << "Tablica po posortowaniu: \n";
     if (choice == 1) {
-        array = sortNonDescending(array, n);
+        sortNonDescending(array, n);
+        printArray(array, n);
+
     }
     else if (choice == 2) {
-
+        sortNonAscending(array, n);
+        printArray(array, n);
     }
     else {
         std::cout << "Niepoprawny wybor\n";
         return -1;
     }
 
-
+    delete[] array;
     return 0;
 }
