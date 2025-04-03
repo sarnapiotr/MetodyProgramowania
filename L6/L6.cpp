70cd4479531ceb3eb1730d501a0710d28f03486a
Piotr Sarna <95252277+sarnapiotr@users.noreply.github.com>
2025-04-03 20:42:47 +0200

    Update L6.cpp

--- L6/L6.cpp
+++ L6/L6.cpp
@@ -21,8 +21,63 @@ void printArray(int* array, int n) {
     std::cout << std::endl;
 }
 
-void mergeSort(int* array, int left, int right) {
+void merge(int* array, int left, int mid, int right) {
+    int leftSize = mid - left + 1;
+    int rightSize = right - mid;
 
+    int* leftArray = new int[leftSize];
+    int* rightArray = new int[rightSize];
+
+    for (int i = 0; i < leftSize; i++) {
+        leftArray[i] = array[left + i];
+    }
+    for (int i = 0; i < rightSize; i++) {
+        rightArray[i] = array[mid + 1 + i];
+    }
+
+    int leftMergeIndex = 0;
+    int rightMergeIndex = 0;
+    int mergedIndex = left;
+
+    while (leftMergeIndex < leftSize && rightMergeIndex < rightSize) {
+        if (leftArray[leftMergeIndex] < rightArray[rightMergeIndex]) {
+            array[mergedIndex] = leftArray[leftMergeIndex];
+            leftMergeIndex++;
+        }
+        else {
+            array[mergedIndex] = rightArray[rightMergeIndex];
+            rightMergeIndex++;
+        }
+
+        mergedIndex++;
+    }
+
+    while (leftMergeIndex < leftSize) {
+        array[mergedIndex] = leftArray[leftMergeIndex];
+        leftMergeIndex++;
+        mergedIndex++;
+
+    }
+
+    while (rightMergeIndex < rightSize) {
+        array[mergedIndex] = rightArray[rightMergeIndex];
+        rightMergeIndex++;
+        mergedIndex++;
+
+    }
+
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+void mergeSort(int* array, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+
+        mergeSort(array, left, mid);
+        mergeSort(array, mid + 1, right);
+        merge(array, left, mid, right);
+    }
 }
 
 int main()
@@ -44,8 +99,9 @@ int main()
 
     std::cout << "Tablica po posortowaniu: \n";
     start = std::chrono::high_resolution_clock::now();
-    mergeSort(array, 0, n);
+    mergeSort(array, 0, n - 1);
     stop = std::chrono::high_resolution_clock::now();
+    printArray(array, n);
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << "Czas dzialania algorytmu wynosi " << duration.count() << " mikrosekund\n";
